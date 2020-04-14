1307317d9ed0e64a6e3f80d42bba1bd45177b5cc
Ruchir Khaitan <ruchir0001@gmail.com>
2020-04-14 11:28:23 -0400

    its compiling

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ extern crate differential_dataflow;
 extern crate timely;
 
 use std::collections::{BTreeSet, HashMap, HashSet};
+use std::collections::btree_set::IntoIter;
 use std::iter::FromIterator;
 
 use differential_dataflow::input::InputSession;
@@ -57,7 +58,7 @@ impl Suited {
         self.current_suitors.insert(suitor);
     }
 
-    fn reject(&mut self) -> Option<BTreeSet<u32>> {
+    fn reject(&mut self) -> Option<IntoIter<u32>> {
         let mut accept_preference = u32::max_value();
         let mut accept_suitor = 0;
 
@@ -73,45 +74,58 @@ impl Suited {
         }
 
         self.current_accept = Some(accept_suitor);
-        let mut ret = std::mem::replace(&mut self.current_suitors, BTreeSet::new());
-        ret.remove(&self.current_accept.expect("current suitor known to exist"));
+        let mut rejections = std::mem::replace(&mut self.current_suitors, BTreeSet::new());
+        rejections.remove(&self.current_accept.expect("current suitor known to exist"));
         self.current_suitors.insert(self.current_accept.expect("current suitor known to exist"));
 
-        return Some(ret);
+        return Some(rejections.into_iter());
     }
 
 }
 
 fn stable_marriages(suitors: &mut [Suitor], suiteds: &mut [Suited]) {
-    let mut unassigned: HashSet<_> = HashSet::from_iter(suitors.iter());
-    let suiteds: HashMap<_ , _> = HashMap::from_iter(suiteds.iter().map(|s| (s.id, s)));
+    let mut unassigned: HashSet<_> = HashSet::from_iter(suitors.iter().map(|s| s.id));
+    let mut suiteds: HashMap<_ , _> = HashMap::from_iter(suiteds.iter_mut().map(|s| (s.id, s)));
+    let mut suitors: HashMap<_ , _> = HashMap::from_iter(suitors.iter_mut().map(|s| (s.id, s)));
 
     while !unassigned.is_empty() {
-        for suitor in unassigned.iter() {
+        for suitor_id in unassigned.iter() {
+            let suitor = suitors.get_mut(&suitor_id);
+
+            if suitor.is_none() {
+                continue;
+            }
+
+            let suitor = suitor.expect("suitor known to exist"); 
+
             let preference = suitor.get_current_preference();
 
             if preference.is_none() {
                 continue;
             }
 
-            let to_propose = suiteds.get(&preference.expect("preference known to exist"));
+            let to_propose = suiteds.get_mut(&preference.expect("preference known to exist"));
 
             if let Some(to_propose) = to_propose {
                 to_propose.add_suitor(suitor.id);
             }
         }
 
-        let unassigned = HashSet::new();
+        unassigned = HashSet::new();
 
-        for (_, suited) in suiteds.iter() {
+        for (_, suited) in suiteds.iter_mut() {
             if let Some(rejections) = suited.reject() {
-                unassigned = unassigned.union(&rejections);
+                unassigned.extend(rejections);
             }
         }
 
-        for suitor in unassigned.iter() {
-            suitor.increment_preference_index();
-        }i
+        for suitor_id in unassigned.iter() {
+            let suitor = suitors.get_mut(&suitor_id);
+
+            if let Some(suitor) = suitor {
+                suitor.increment_preference_index();
+            }
+        }
     }
 
     println!("{:?}", suiteds);
